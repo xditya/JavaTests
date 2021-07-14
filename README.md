53b4c463fab2e830e76485f042413742c0e2f710
Aditya <xditya@ultroid.tech>
2021-07-14 11:24:24 +0530

    Heroku support | Dockerise the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaTests
+
+As the name says (or if you are dumb enough not to understand it), this is a testing repository, where I play around with java, to try to make a telegram bot.
+
+Ok thanks, [bye](https://heroku.com/deploy).
+
+Btw, if you need to learn/help with this, feel free to dm me on telegram, [@xditya](https://t.me/xditya)!
--- src/main/java/com/xditya/plugins/start.java
+++ src/main/java/com/xditya/plugins/start.java
@@ -5,9 +5,9 @@ import com.xditya.bot;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class start extends bot implements pluginHandler{
+public class start extends bot implements pluginHandler {
     public void commandInvoked(Update update, String command, String args) {
-        if(command.equals("start") || command.equals("start" + "@" + getBotUsername()))
+        if (command.equals("start") || command.equals("start" + "@" + getBotUsername()))
             sendmsg(update.getMessage().getChatId().toString(), "Hello. I'm a test bot. Please get lost.");
-        }
     }
+}
