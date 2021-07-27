015765872cd1cc00b7914fa8dcc275f45483d1d4
Aditya <xditya@ultroid.tech>
2021-07-27 09:18:47 +0530

    evals

--- src/main/java/com/xditya/plugins/eval.java
+++ src/main/java/com/xditya/plugins/eval.java
@@ -1,10 +1,10 @@
 package com.xditya.plugins;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
 import com.xditya.helpers.Config;
-import com.xditya.helpers.send;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -12,33 +12,65 @@ import com.xditya.pluginHandler;
 import com.xditya.bot;
 
 public class eval extends bot implements pluginHandler {
+    private static String defpath = "src/main/java/com/xditya/run.java";
+
     public void commandInvoked(Update update, String command, String args) {
         String chatID = update.getMessage().getChatId().toString();
+        int msgID;
         if (command.equals("eval") || command.equalsIgnoreCase("eval@" + getBotUsername())) {
             if (args.length() == 0) {
                 sendMessage(chatID, "Please use " + Config.handler + "eval <a command>.");
                 return;
             }
-            boolean done = WriteToFile(args);
+            boolean done = WriteToFile(
+                    "class run {\n   public static void main(String args[]) {\n      " + args + "\n   }\n}");
             if (done) {
-                sendMessage(chatID, "Done. Wrote to file.");
-                new send().file(update, "run.java", "Done. Written to file.");
-                Path fileToDeletePath = Paths.get("run.java");
+                msgID = sendMessage(chatID, "Done. Wrote to file.").getMessageId();
+                Process process;
                 try {
-                    Files.delete(fileToDeletePath);
-                    sendMessage(chatID, "Deleted File.");
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    process = Runtime.getRuntime().exec("java " + defpath);
+                    InputStream stdout = process.getInputStream();
+                    InputStream stderr = process.getErrorStream();
+                    BufferedReader err = new BufferedReader(new InputStreamReader(stderr, StandardCharsets.UTF_8));
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
+                    String line, ln, output = "";
+                    try {
+                        output += "*OUTPUT:*\n";
+                        while ((line = reader.readLine()) != null) {
+                            output += "`" + line + "`\n";
+                        }
+                    } catch (IOException e) {
+                        output += "*Exception in reading output:*\n`" + e.toString() + "`\n";
+                    }
+                    output += "\n";
+                    try {
+                        output += "*ERROR:*\n";
+                        while ((ln = err.readLine()) != null) {
+                            output += "`" + ln + "`\n";
+                        }
+                    } catch (IOException e) {
+                        output += "*Exception in reading error output:*\n`" + e.toString() + "`\n";
+                    }
+                    editMessage(chatID, msgID, output);
+                } catch (Exception e) {
+                    System.out.println("Exception Raised: " + e.toString());
                 }
+            }
+
+            Path fileToDeletePath = Paths.get(defpath);
+            try {
+                Files.delete(fileToDeletePath);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public static Boolean WriteToFile(String message) {
         try {
-            File file = new File("run.java");
+            File file = new File(defpath);
             if (file.createNewFile()) {
-                FileWriter myWriter = new FileWriter("run.java");
+                FileWriter myWriter = new FileWriter(defpath);
                 myWriter.write(message);
                 myWriter.close();
                 return true;
